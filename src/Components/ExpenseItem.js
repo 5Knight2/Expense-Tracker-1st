60d72ca48cfbacaf3679675411cd31a77992f742
5Knight2 <pratiktarale258@gmail.com>
2024-01-25 21:30:38 +0530

    variables added

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import './ExpenseItem.css'
 function ExpenseItem(){
-   
+    const Title='Food'
+    const Date1=new Date(2024,1,25);
+    const Amount=2420;
+    const location="Akola"
     return (
+        
         <div className='expense-item'>
-        <div >25-01-24</div>
-        <div className='expense-item__description'><h2>Food</h2></div>
-        <div className='expense-item__price'>240</div>
+        <div >{Date1.toDateString()}</div>
+        <div className='expense-item__description'><h2>  {Title}</h2></div>
+        <div className='expense-item__description'><h2>  {location}</h2></div>
+        <div className='expense-item__price'>{Amount}</div>
         </div>
         
         );
