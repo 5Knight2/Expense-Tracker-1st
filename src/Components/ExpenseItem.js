135a898b359291ce2dddcb958bd82459aa4aeee5
5Knight2 <pratiktarale258@gmail.com>
2024-01-25 21:17:26 +0530

    Css added to expense item

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,8 +1,14 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
    
     return (
-        <h2>Expense Item</h2>
-    );
+        <div className='expense-item'>
+        <div >25-01-24</div>
+        <div className='expense-item__description'><h2>Food</h2></div>
+        <div className='expense-item__price'>240</div>
+        </div>
+        
+        );
 }
 
 export default ExpenseItem; 
