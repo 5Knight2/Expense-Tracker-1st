aeed1c5a41e9cb18fbe02b76861b0a466aaba623
5Knight2 <pratiktarale258@gmail.com>
2024-01-26 20:04:15 +0530

    Delete btn added

--- src/Components/Expenses/ExpenseDetails.js
+++ src/Components/Expenses/ExpenseDetails.js
@@ -1,6 +1,10 @@
 
 const ExpenseDetails=(props)=>{
 
+  const deleteClicked=()=>{
+    console.log('clicked');
+  }
+
     return (
     <div className="expense-item__description">
         
@@ -10,6 +14,7 @@ const ExpenseDetails=(props)=>{
     <h2> {props.location} </h2>
 
   <div className="expense-item__price">{props.amount}</div>
+  <button className="delete" onClick={deleteClicked}> Delete</button>
   </div>);
 }
 
