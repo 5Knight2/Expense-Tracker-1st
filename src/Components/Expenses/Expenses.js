8ad79c9d42bf36a2e009e213bfe9b755c033440c
5Knight2 <pratiktarale258@gmail.com>
2024-03-11 10:43:13 +0530

    Condition added for no zero exceptions

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -12,12 +12,16 @@ const [filteredYear,setFilteredYear]=useState('2020');
         setExpenseArray(props.items.filter((currentExpense)=>currentExpense.date.getFullYear()==selectedYear))
     }
 
+    let expensesContent=<p>No expenses available</p>
+
+    if(expenseArray.length>0) expensesContent=expenseArray.map((expense)=>{
+        return <ExpenseItem key={expense.id} title={expense.title} price={expense.amount} date={expense.date} location={expense.location}></ExpenseItem>
+    })
+
     return(
         <Card className="expenses">
             <ExpensesFilter value={filteredYear} onChangeFilter={filterchangeHandler}></ExpensesFilter>
-            {expenseArray.map((expense)=>{
-                return <ExpenseItem key={expense.id} title={expense.title} price={expense.amount} date={expense.date} location={expense.location}></ExpenseItem>
-            })}
+            {expensesContent}
         {/* <ExpenseItem title={props.items[0].title} price={props.items[0].amount} date={props.items[0].date} location={props.items[0].location }></ExpenseItem>
         <ExpenseItem title={props.items[1].title} price={props.items[1].amount} date={props.items[1].date} location={props.items[1].location }></ExpenseItem>
         <ExpenseItem title={props.items[2].title} price={props.items[2].amount} date={props.items[2].date} location={props.items[2].location }></ExpenseItem> */}
