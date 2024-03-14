22aaab632f03d79dc5097b1fa68b1e248f4b0650
5Knight2 <pratiktarale258@gmail.com>
2024-03-14 10:54:04 +0530

    Bar Chart

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React,{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from '../UI/Card'
+import ExpensesChart from "./ExpensesChart";
 const Expenses=(props)=>{
 const [expenseArray,setExpenseArray]=useState(props.items);
 
@@ -17,11 +18,15 @@ const [filteredYear,setFilteredYear]=useState('2020');
     if(expenseArray.length>0) expensesContent=expenseArray.map((expense)=>{
         return <ExpenseItem key={expense.id} title={expense.title} price={expense.amount} date={expense.date} location={expense.location}></ExpenseItem>
     })
-
+    
     return(
         <Card className="expenses">
+            
             <ExpensesFilter value={filteredYear} onChangeFilter={filterchangeHandler}></ExpensesFilter>
-            {expensesContent}
+            <ExpensesChart expenses={expenseArray} />
+            {expenseArray.map((expense)=>{
+                return <ExpenseItem key={expense.id} title={expense.title} price={expense.amount} date={expense.date} location={expense.location}></ExpenseItem>
+            })}
         {/* <ExpenseItem title={props.items[0].title} price={props.items[0].amount} date={props.items[0].date} location={props.items[0].location }></ExpenseItem>
         <ExpenseItem title={props.items[1].title} price={props.items[1].amount} date={props.items[1].date} location={props.items[1].location }></ExpenseItem>
         <ExpenseItem title={props.items[2].title} price={props.items[2].amount} date={props.items[2].date} location={props.items[2].location }></ExpenseItem> */}
